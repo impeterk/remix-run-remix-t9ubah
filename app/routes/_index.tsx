ea40bef69231f0c512f8fefc24a70e88b2bf2a76
Peter Kudelas <storm.sk123@gmail.com>
2024-10-16 09:33:26 +0200

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import {json} from '@remix-run/node";
+import {json} from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
